e82fd0b6f7d5591aacdff4ab90187b42414cc97e
Prelang Builder <builder@prelang.com>
2014-05-10 03:04:06 +0000

    Generated controller for root route with 'index' action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 DemoForum::Application.routes.draw do
+  get "landings/index"
   resources :posts
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords", omniauth_callbacks: "users/omniauth_callbacks"}, skip: [:sessions, :registrations]
